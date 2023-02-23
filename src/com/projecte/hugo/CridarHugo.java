4762abd225811948d67e31c069cac9a1767f4d1a
Hugo Sanchis <hugolluissimarro03@gmail.com>
2023-02-23 21:05:00 +0100

    Metode crida Hugo fet

---
+++ src/com/projecte/hugo/CridarHugo.java
@@ -0,0 +1,8 @@
+package com.projecte.hugo;
+
+public class CridarHugo {
+	// Metode per mostrar el nom de Hugo
+	public void cridarHugo() {
+		System.out.println("Hugo Sanchis");
+	}
+}
--- src/com/projecte/main/ProgramaPrincipal.java
+++ src/com/projecte/main/ProgramaPrincipal.java
@@ -1,5 +1,6 @@
 package com.projecte.main;
 
+import com.projecte.hugo.CridarHugo;
 import com.projecte.ionatan.CridarIonatan;
 
 public class ProgramaPrincipal {
@@ -7,10 +8,12 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		// Importar noms de membres del grup
 		CridarIonatan cridarIonatan = new CridarIonatan();
+		CridarHugo cridarHugo = new CridarHugo();
 
-		
 		// Cridar a membres del grup
+		System.out.println("Membres: ");
 		cridarIonatan.cridarIonatan();
+		cridarHugo.cridarHugo();
 
 	}
 
