853c7d08d710e3d24c78732d5a448cc07f473506
Hugo Sanchis <hugolluissimarro03@gmail.com>
2023-02-23 21:17:53 +0100

    Branca Hugo creada

--- src/com/projecte/hugo/CridarHugo.java
+++ src/com/projecte/hugo/CridarHugo.java
@@ -2,7 +2,7 @@ package com.projecte.hugo;
 
 public class CridarHugo {
 	// Metode per mostrar el nom de Hugo
-	public void cridarHugo() {
+	public void cridarHugo() { 
 		System.out.println("Hugo Sanchis");
 	}
 }
