9b3851973787830ba1cf87a86c8e408fb1479b44
halo3000 <halo3000000@gmail.com>
2023-02-24 13:48:13 +0100

    metode juanen creat

--- src/com/projecte/main/ProgramaPrincipal.java
+++ src/com/projecte/main/ProgramaPrincipal.java
@@ -3,6 +3,7 @@ package com.projecte.main;
 import com.projecte.christian.CridarChristian;
 import com.projecte.hugo.CridarHugo;
 import com.projecte.ionatan.CridarIonatan;
+import com.projecte.juanen.CridarJuanen;
 
 public class ProgramaPrincipal {
 
@@ -11,12 +12,14 @@ public class ProgramaPrincipal {
 		CridarIonatan cridarIonatan = new CridarIonatan();
 		CridarChristian cridarChristian = new CridarChristian();
 		CridarHugo cridarHugo = new CridarHugo();
+		CridarJuanen cridarJuanen=new CridarJuanen();
 
 		// Cridar a membres del grup
 		System.out.println("Membres: ");
 		cridarIonatan.cridarIonatan();
 		cridarChristian.cridarChristian();
 		cridarHugo.cridarHugo();
+		cridarJuanen.CridarJuanen();
 	}
 
 }
