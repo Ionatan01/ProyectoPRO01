621acab9c66bb080ab186e45972d9e319a40fa23
Ionatan Muntean <ionatanmunteanonti@gmail.com>
2023-03-07 22:05:30 +0100

    Creado metodo crear usuario
    Creado metodo crear usuario

--- src/com/projecte/ionatan/GuardarObjeto.java
+++ src/com/projecte/ionatan/GuardarObjeto.java
@@ -6,64 +6,82 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Date;
+import java.util.Scanner;
 
 import com.projecte.main.Usuari;
 
 public class GuardarObjeto {
 
 	public static void main(String[] args) {
-		Usuari admin = new Usuari(1, "admin", "admin", "admin@gmail.com", "1234", "Ontinyent",
-				"USUARI_ADMIN", null);
-		Usuari prueba = new Usuari(2, "otro", "otro", "otro@gmail.com", "1234", "Ontinyent",
-				"USUARI_NORMAL", null);
+		Usuari prueba = new Usuari(2, "otro", "otro", "otro@gmail.com", "1234", "Ontinyent", null);
 		GuardarObjeto programa = new GuardarObjeto();
-		
-		//programa.guardarInfoUsuario(admin);
-		//programa.guardarInfoUsuario(prueba);
-		
-		System.out.println(prueba.getNomCorreuElectronic());
-		
 
-		//int id, String nom, String cognoms, String correuElectronic, String contraseña, String poblacio,
-		//String rol, Date dataNaixement
-	}
-	
-	public void guardarObjeto(Usuari p) {
+		programa.guardarInfoUsuario(prueba);
+		//programa.guardarInfoUsuario(prueba);
+
+		//System.out.println(prueba.getNomCorreuElectronic());
+
+		// int id, String nom, String cognoms, String correuElectronic, String
+		// contraseña, String poblacio,
+		// String rol, Date dataNaixement
 		
+		// Enum rol
+		
+		// Metodo id
 	}
-	
+
+//	public void guardarObjeto(Usuari p) {
+//
+//	}
+
 	public void guardarInfoUsuario(Usuari p) {
 		FileWriter fw;
 		// Crear metodo para retornar solo parte de delante del correo
-		File carpeta = new File(p.getId()+p.getNomCorreuElectronic());
+		File carpeta = new File("usuarios/" + p.getId() + p.getNomCorreuElectronic());
 		File peliculas = new File(carpeta + "/p.dades");
 		File actores = new File(carpeta + "/a.dades");
 		File directores = new File(carpeta + "/d.dades");
-		
-		
-		
-		
+
 		try {
-			fw = new FileWriter("dades/usuaris.txt",true);
+			fw = new FileWriter("dades/usuaris.txt", true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.append(p.toStringParaGuardar()); // Ponerlo en el buffer
 			bw.newLine(); // Insertar salto de linea
-			bw.flush(); /// Guardar  en fichero
-			
+			bw.flush(); /// Guardar en fichero
+
 			bw.close();
-			
-			if (!carpeta.isFile()) {
-				carpeta.getParentFile().mkdirs();
-				
+
+			if (!carpeta.isDirectory()) {
+				carpeta.mkdir();
+			}
+			if (!peliculas.isFile()) {
+				peliculas.createNewFile();
+			}
+			if (!actores.isFile()) {
+				actores.createNewFile();
+			}
+			if (!directores.isFile()) {
+				directores.createNewFile();
 			}
 			
-			// Falta crear carpeta
-			
+
+
 			System.out.println("Usuario " + p.getNom() + " guardado correctamente.");
 		} catch (IOException e) {
 			System.out.println("No se ha podido escribir en el fichero: " + e);
 		}
 
+	}
+	
+	public int siguienteId() {
+		int ultimoIdAsignado = 0;
+		File ficheroAEditar = new File("dades/usuaris.txt");
+		Scanner escaner = new Scanner(System.in);
+		
+		
+		
+		
+		return ultimoIdAsignado + 1;
 	}
 
 }
--- src/com/projecte/main/Usuari.java
+++ src/com/projecte/main/Usuari.java
@@ -1,11 +1,16 @@
 package com.projecte.main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Scanner;
 
+import com.projecte.ionatan.GuardarObjeto;
 import com.projecte.utils.*;
 
 public class Usuari implements Accions, Serializable {
@@ -13,6 +18,9 @@ public class Usuari implements Accions, Serializable {
 	private static int contador = 0;
 
 	// Atributs
+	public enum tipusRol {
+		ROL_ADMIN, ROL_USUARI
+	}
 
 	private int id;
 	private String nom;
@@ -20,7 +28,7 @@ public class Usuari implements Accions, Serializable {
 	private String correuElectronic;
 	private String contraseña;
 	private String poblacio;
-	private String rol = "ROL_USUARI";
+	private tipusRol rol;
 	private Date dataNaixement;
 
 	// constructor
@@ -34,11 +42,9 @@ public class Usuari implements Accions, Serializable {
 		this.poblacio = poblacio;
 		this.dataNaixement = dataNaixement;
 	}
-	
-	
 
 	public Usuari(int id, String nom, String cognoms, String correuElectronic, String contraseña, String poblacio,
-			String rol, Date dataNaixement) {
+			Date dataNaixement) {
 		super();
 		this.id = id;
 		this.nom = nom;
@@ -46,96 +52,88 @@ public class Usuari implements Accions, Serializable {
 		this.correuElectronic = correuElectronic;
 		this.contraseña = contraseña;
 		this.poblacio = poblacio;
-		this.rol = rol;
+		this.rol = tipusRol.ROL_USUARI;
 		this.dataNaixement = dataNaixement;
 	}
-
-
 
 	// Metodos
 
 	public static void Login() {
 		Scanner leerScanner = new Scanner(System.in);
-		  String linea;
-		  boolean contraseñaEncontrada = false;
-	        boolean usuarioEncontrado=false;
+		String linea;
+		boolean contraseñaEncontrada = false;
+		boolean usuarioEncontrado = false;
 		System.out.print("Nombre: ");
 		String nomString = Metodos.demanarNom(leerScanner);
 		System.out.print("Contraseña: ");
 		String contrasenaLogin = Metodos.DemanarContrasenaLogin(leerScanner);
 
 		try (BufferedReader br = new BufferedReader(new FileReader("usuarios.txt"))) {
-            while ((linea = br.readLine()) != null) {
-                if (linea.contains(nomString) && linea.contains(contrasenaLogin)) {
-  		 usuarioEncontrado = true;
-                    contraseñaEncontrada = true;
-                    break;
-                }
-            }
-        		 
-		}catch (Exception e) {
+			while ((linea = br.readLine()) != null) {
+				if (linea.contains(nomString) && linea.contains(contrasenaLogin)) {
+					usuarioEncontrado = true;
+					contraseñaEncontrada = true;
+					break;
+				}
+			}
+
+		} catch (Exception e) {
 			System.out.println("Error:: Usuario no encontrado");
 		}
 	}
-	
-	public  void mostrarMenu() {
+
+	public void mostrarMenu() {
 		Scanner leerScanner = new Scanner(System.in);
-		int opcion =0;
-		 System.out.println("¡Bienvenido, " + nom + "!");
-		 while (true) {
-	            System.out.println("¿Qué te gustaría hacer?");
-	            System.out.println("1. Ver listado de películas");
-	            System.out.println("2. Ver listado de actores");
-	            System.out.println("3. Ver listado de directores");
-	            System.out.println("4. Añadir película");
-	            System.out.println("5. Añadir actor");
-	            System.out.println("6. Añadir director");
-	            System.out.println("7. Salir");
+		int opcion = 0;
+		System.out.println("¡Bienvenido, " + nom + "!");
+		while (true) {
+			System.out.println("¿Qué te gustaría hacer?");
+			System.out.println("1. Ver listado de películas");
+			System.out.println("2. Ver listado de actores");
+			System.out.println("3. Ver listado de directores");
+			System.out.println("4. Añadir película");
+			System.out.println("5. Añadir actor");
+			System.out.println("6. Añadir director");
+			System.out.println("7. Salir");
 
-	          
-	            System.out.print("Opción: ");
-	            opcion=leerScanner.nextInt();
+			System.out.print("Opción: ");
+			opcion = leerScanner.nextInt();
 
-	       
-	            switch (opcion) {
-	                case 1:
-	                	//mostrarPeliculas(nom);
-	                    System.out.println("Mostrando listado de películas...");
-	                    break;
-	                case 2:
-	                	//mostrarActores(nom);
-	                    System.out.println("Mostrando listado de actores...");
-	                    break;
-	                case 3:
-	                	//mostrarDirectores(nom);
-	                    System.out.println("Mostrando listado de directores...");
-	                    break;
-	                case 4:
-	                	//AñadirPeliculas(nom);
-	                    System.out.println("Añadiendo película...");
-	                    break;
-	                case 5:
-	                	//AñadirActor(nom);
-	                    System.out.println("Añadiendo actor...");
-	                    break;
-	                case 6:
-	                	//AñadirDirector(nom);
-	                    System.out.println("Añadiendo director...");
-	                    break;
-	                case 7:
-	                    System.out.println("¡Hasta luego!");
-	                    return;
-	                default:
-	                    System.out.println("Opción inválida. Por favor, elige una opción válida.");
-	                    break;
-	            }
-	        }
-		
-		
+			switch (opcion) {
+			case 1:
+				// mostrarPeliculas(nom);
+				System.out.println("Mostrando listado de películas...");
+				break;
+			case 2:
+				// mostrarActores(nom);
+				System.out.println("Mostrando listado de actores...");
+				break;
+			case 3:
+				// mostrarDirectores(nom);
+				System.out.println("Mostrando listado de directores...");
+				break;
+			case 4:
+				// AñadirPeliculas(nom);
+				System.out.println("Añadiendo película...");
+				break;
+			case 5:
+				// AñadirActor(nom);
+				System.out.println("Añadiendo actor...");
+				break;
+			case 6:
+				// AñadirDirector(nom);
+				System.out.println("Añadiendo director...");
+				break;
+			case 7:
+				System.out.println("¡Hasta luego!");
+				return;
+			default:
+				System.out.println("Opción inválida. Por favor, elige una opción válida.");
+				break;
+			}
+		}
+
 	}
-	
-
-	
 
 	public static void registro() {
 		Scanner leerScanner = new Scanner(System.in);
@@ -153,13 +151,18 @@ public class Usuari implements Accions, Serializable {
 		Date fechaNaixement = Metodos.demanarFecha(leerScanner);
 
 		try {
-			Usuari usuari = new Usuari(contador, nomString, cognomString, correoString, contrasenaString, poblacioString,
-					contrasenaString, fechaNaixement);
-			if (Metodos.CrearDirectorioUsuario(usuari.nom) == true) {
-				System.out.println("Usuario ya existe");
-			} else {
-				Metodos.CrearFitxerosUsuario(usuari.nom);
-			}
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+			Usuari usuari = new Usuari(2, nomString, cognomString, correoString, contrasenaString, poblacioString, fechaNaixement);
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+			// Cambiar 2 per metode contador
+
+			crear(usuari);
+			
 			System.out.println(usuari.toString());
 
 		} catch (Exception e) {
@@ -196,12 +199,12 @@ public class Usuari implements Accions, Serializable {
 
 	public String getNomCorreuElectronic() {
 		String correo = this.getCorreuElectronic();
-		String [] correoSeparado;
-		
+		String[] correoSeparado;
+
 		correoSeparado = correo.split("@");
 		return correoSeparado[0];
 	}
-	
+
 	public void setCorreuElectronic(String correuElectronic) {
 		this.correuElectronic = correuElectronic;
 	}
@@ -223,11 +226,7 @@ public class Usuari implements Accions, Serializable {
 	}
 
 	public String getRol() {
-		return rol;
-	}
-
-	public void setRol(String rol) {
-		this.rol = rol;
+		return "" + rol;
 	}
 
 	public Date getDataNaixement() {
@@ -252,18 +251,48 @@ public class Usuari implements Accions, Serializable {
 				+ ", contraseña=" + contraseña + ", poblacio=" + poblacio + ", rol=" + rol + ", dataNaixement="
 				+ dataNaixement + "]";
 	}
-	
+
 	public String toStringParaGuardar() {
-		return id + "," + nom + "," + cognoms + "," + correuElectronic
-				+ "," + contraseña + "," + poblacio + "," + rol + ","
-				+ dataNaixement;
+		return id + ":" + nom + ":" + cognoms + ":" + correuElectronic + ":" + contraseña + ":" + poblacio + ":" + rol
+				+ ":" + dataNaixement;
 	}
 
 	// Metodos interfaz
 
-	@Override
-	public void crear() {
-		// TODO Auto-generated method stub
+	public static void crear(Usuari p) {
+		FileWriter fw;
+		// Crear metodo para retornar solo parte de delante del correo
+		File carpeta = new File("usuarios/" + p.getId() + p.getNomCorreuElectronic());
+		File peliculas = new File(carpeta + "/p.dades");
+		File actores = new File(carpeta + "/a.dades");
+		File directores = new File(carpeta + "/d.dades");
+
+		try {
+			fw = new FileWriter("dades/usuaris.txt", true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.append(p.toStringParaGuardar()); // Ponerlo en el buffer
+			bw.newLine(); // Insertar salto de linea
+			bw.flush(); /// Guardar en fichero
+
+			bw.close();
+
+			if (!carpeta.isDirectory()) {
+				carpeta.mkdir();
+			}
+			if (!peliculas.isFile()) {
+				peliculas.createNewFile();
+			}
+			if (!actores.isFile()) {
+				actores.createNewFile();
+			}
+			if (!directores.isFile()) {
+				directores.createNewFile();
+			}
+
+			System.out.println("Usuario " + p.getNom() + " guardado correctamente.");
+		} catch (IOException e) {
+			System.out.println("No se ha podido escribir en el fichero: " + e);
+		}
 
 	}
 
@@ -283,6 +312,12 @@ public class Usuari implements Accions, Serializable {
 	public void modificar() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void crear() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
