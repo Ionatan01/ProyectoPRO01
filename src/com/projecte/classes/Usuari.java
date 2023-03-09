2ca87993dc6d2c0f200f6ff958ed2da4a7c79840
HugoSanchis16 <hugolluissimarro03@gmail.com>
2023-03-09 14:42:14 +0100

    Prueba

--- src/com/projecte/classes/Usuari.java
+++ src/com/projecte/classes/Usuari.java
@@ -8,8 +8,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
